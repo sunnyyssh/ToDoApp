f98ac19c26c4b5d99eeb6d0388d7812aaa4ab627
sunnyyssh <blit228@mail.ru>
2024-04-14 13:48:13 +0300

    Implemented ViewModel.

---
+++ ToDoApp/ViewModel/Context.cs
@@ -0,0 +1,24 @@
+﻿using ToDoApp.Model;
+
+namespace ToDoApp.ViewModel;
+
+public sealed class Context
+{
+    public ViewContextSide ViewSide { get; }
+    
+    public ModelContextSide ModelSide { get; }
+    
+    public Context(Settings initSettings, IEnumerable<ToDoTask> initTasks)
+    {
+        ArgumentNullException.ThrowIfNull(initSettings, nameof(initSettings));
+        ArgumentNullException.ThrowIfNull(initTasks, nameof(initTasks));
+
+        var collectionChanger = new TasksCollectionChanger();
+        var tasksCollection = new TasksCollection(initTasks, collectionChanger);
+
+        ViewSide = new ViewContextSide(initSettings, tasksCollection, collectionChanger);
+        
+        ModelSide = new ModelContextSide(initSettings, tasksCollection);
+        ModelSide.BindViewSide(ViewSide, collectionChanger);
+    }
+}
---
+++ ToDoApp/ViewModel/Settings.cs
@@ -0,0 +1,13 @@
+﻿using ToDoApp.View;
+
+namespace ToDoApp.ViewModel;
+
+public sealed class Settings
+{
+    public Theme Theme { get; }
+
+    public Settings(Theme theme)
+    {
+        Theme = theme;
+    }
+}
